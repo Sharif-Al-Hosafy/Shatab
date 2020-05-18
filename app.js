31ae5d8f122424de67e80f245038e41dee9fd2db
Sharif_Hosafy <Sherifismail44@gmail.com>
2020-05-18 02:47:30 +0200

    Add Frontend Features

--- app.js
+++ app.js
@@ -2,62 +2,53 @@ let express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose");
-  passport    =require("passport"),
-  LocalStrategy=require("passport-local"),
-  Constructor  =require("./models/constructor"),
-  Comment      =require("./models/comment"),
-  User         =require("./models/user");
-  Request      =require("./models/constructorrequest"),
-  Category= require("./models/category"),
-  Product =require("./models/product"),
-  Company =require("./models/company");
-  session     =require("express-session");
-  MongoStore  =require("connect-mongo")(session);
+(passport = require("passport")),
+  (LocalStrategy = require("passport-local")),
+  (Constructor = require("./models/constructor")),
+  (Comment = require("./models/comment")),
+  (User = require("./models/user"));
+(Request = require("./models/constructorrequest")),
+  (Category = require("./models/category")),
+  (Product = require("./models/product")),
+  (Company = require("./models/company"));
+session = require("express-session");
+MongoStore = require("connect-mongo")(session);
 
+//requiring routes
+var authRoutes = require("./routes/user");
+var categoryRoutes = require("./routes/category");
+var commentRoutes = require("./routes/comments");
+var constructorRoutes = require("./routes/constructors");
+var requestRoutes = require("./routes/request");
+var categoryRoutes = require("./routes/category");
 
+//PASSPORT CONFIGUARTION
+app.use(
+  session({
+    secret: "shatab",
+    ressave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 180 * 60 * 1000 },
+  })
+);
 
-   //requiring routes
-  var authRoutes = require("./routes/user");
-  var categoryRoutes=require("./routes/category");
-  var commentRoutes = require("./routes/comments");
-  var constructorRoutes = require("./routes/constructors");
-  var requestRoutes = require("./routes/request");
-  var categoryRoutes=require("./routes/category");
-
-
-  //PASSPORT CONFIGUARTION
-  app.use(session({
-    secret:"shatab",
-    ressave:false,
-    saveUninitialized:false,
-    store:new MongoStore({mongooseConnection:mongoose.connection}),
-    cookie:{maxAge:180 * 60 * 1000}
- }));
-   
- app.use(passport.initialize());
- app.use(passport.session());
- passport.use(new LocalStrategy(User.authenticate()));
- passport.serializeUser(User.serializeUser());
- passport.deserializeUser(User.deserializeUser());
-
-
-  
-
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 mongoose.connect("mongodb://localhost/shatabProj");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public")); //mention the public directory from which we are serving the static files
 
-
-
-
 app.use(authRoutes);
 app.use(constructorRoutes);
-app.use("/constructors/:id/comments",commentRoutes);
+app.use("/constructors/:id/comments", commentRoutes);
 app.use(requestRoutes);
 app.use(categoryRoutes);
-
 
 /*Constructor.create({
    name:"c4",
@@ -67,7 +58,6 @@ app.use(categoryRoutes);
   phonenumber:"01233336633",
  pricePerHour:"100LE",
 });*/
-
 
 app.get("/", function (req, res) {
   res.render("Landing");
